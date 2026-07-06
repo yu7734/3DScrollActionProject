4575613adffedb7e23efaf4cd3e6dde26babd4c5
yu7734 <yuichi.7734@gmail.com>
2026-07-06 10:31:09 +0900

    スクリプト作成
    fallStateを作成した

---
+++ Assets/Scripts/PlayerFallState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerFallState : PlayerStateBase
+{
+
+    public PlayerFallState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+    
+    public override void Enter()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit() { }
+}
