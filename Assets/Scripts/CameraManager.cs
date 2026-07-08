41669d99a5d758fd0f736925f3300369bac70d02
yu7734 <yuichi.7734@gmail.com>
2026-07-08 23:54:01 +0900

    chinemachineを使ったプレイヤー追従カメラ
    chinemachineを使ったプレイヤー追従カメラ
    を追加した

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [SerializeField, Tooltip("プレイヤーのオブジェクトを保持")] private Transform player;
     [SerializeField] private float cameraSmooth;
+    //プレイヤーとカメラの位置
     private Vector3 offset;
+    [SerializeField, Tooltip("カメラのZ座標")] private float cameraOffset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +21,8 @@ public class CameraManager : MonoBehaviour
 
     private void LateUpdate()
     {
-        Vector3 desirePosition = player.position + offset;
-        //Vector3 smoothedPosition = Vector3.Lerp()
+        Vector3 desirePosition = player.transform.position + offset;
+        desirePosition.z = cameraOffset;
+        transform.position = desirePosition;
     }
 }
