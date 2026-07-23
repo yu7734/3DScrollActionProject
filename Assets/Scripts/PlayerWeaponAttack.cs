2c624931fec803a5576ae040f2b10c6b6a65f56e
yu7734 <yuichi.7734@gmail.com>
2026-07-24 01:30:55 +0900

    武器に触れたら敵を消滅
    武器に触れたらEnemyタグが付いている敵を消滅するようにした

--- Assets/Scripts/PlayerWeaponAttack.cs
+++ Assets/Scripts/PlayerWeaponAttack.cs
@@ -13,4 +13,12 @@ public class PlayerWeaponAttack : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
