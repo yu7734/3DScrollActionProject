7669fc88900a26bc3bda614546d1f0bc3c27ebb6
yu7734 <yuichi.7734@gmail.com>
2026-08-12 15:32:16 +0900

    rayの長さを変数にした
    rayの長さを変数にしてunityのinspector内で変更できるようにした

--- Assets/Scripts/SlimeEnemy.cs
+++ Assets/Scripts/SlimeEnemy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SlimeEnemy : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rayDistance;
     private Rigidbody rigidbody;
     private Vector3 moveDirection;//敵の進行方向
 
@@ -38,8 +39,8 @@ public class SlimeEnemy : MonoBehaviour
         Vector2 halfSize = transform.lossyScale / 2;//オブジェクトの2分１のサイズの変数
         int layerMask = LayerMask.GetMask("Floor");//床のレイヤーを取得する変数
         RaycastHit hit;
-        Debug.DrawRay(transform.position, moveDirection, Color.red);// Sceneビューでデバッグ用にRayを可視化
-        if (!Physics.Raycast(transform.position, moveDirection, out hit, 1)) return;
+        Debug.DrawRay(transform.position, moveDirection * rayDistance, Color.red);// Sceneビューでデバッグ用にRayを可視化
+        if (!Physics.Raycast(transform.position, moveDirection, out hit, rayDistance, layerMask)) return;
         if (hit.transform.tag == "Floor")
             moveDirection = -moveDirection;//壁があれば逆方向に向きを変える
     }
