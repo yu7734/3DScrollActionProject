e10fc213b69115705fc3b4a326595f796a8e5063
yu7734 <yuichi.7734@gmail.com>
2026-08-16 13:47:42 +0900

    死亡ステート作成
    死亡ステートを追加して、プレイヤーのHPが0になったら非表示にする

--- Assets/Scripts/Player/DamageState/NormalState.cs
+++ Assets/Scripts/Player/DamageState/NormalState.cs
@@ -28,6 +28,15 @@ public class NormalState : PlayerStateBase
         if (hit.gameObject.tag != "Enemy") return;//“G‚É“–‚½‚Á‚½‚ç
         damageStateMachine.PlayerHP--;//HP‚ðŒ¸‚ç‚·
         Debug.Log(damageStateMachine.PlayerHP);
-        damageStateMachine.SwicthState(typeof(DamagedState));
+        if (damageStateMachine.PlayerHP <= 0)
+            damageStateMachine.SwicthState(typeof(DeadState));//‘Ì—Í‚ª0‚È‚çŽ€–S
+        else
+            damageStateMachine.SwicthState(typeof(DamagedState));
+    }
+
+    private void PlayerDie()
+    {
+        if (!(damageStateMachine.PlayerHP <= 0)) return;
+        damageStateMachine.SwicthState(typeof(DeadState));//‘Ì—Í‚ª0‚È‚çŽ€–S
     }
 }
