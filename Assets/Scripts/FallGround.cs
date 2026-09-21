b6818dfaa0d7706fddfdb05ce7bf051bd01fa566
yu7734 <yuichi.7734@gmail.com>
2026-09-21 23:27:27 +0900

    時間がたったら落下、さらに時間がたったら床を消す

--- Assets/Scripts/FallGround.cs
+++ Assets/Scripts/FallGround.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System;
 using UnityEngine;
 
@@ -25,6 +26,18 @@ public class FallGround : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
-        rigidbody.isKinematic = false;
+        Fall();
+    }
+
+    async UniTask Fall()
+    {
+        //1秒待ってから落下
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
+        if (rigidbody != null)
+            rigidbody.isKinematic = false;
+
+        //３秒経ったらオブジェクト削除
+        await UniTask.Delay(TimeSpan.FromSeconds(3));
+        Destroy(this.gameObject);
     }
 }
